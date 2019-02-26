0ff7e9178e18c1dc33574e8f28d1379a24737f45
KANKANALA <naveen-chary.kankanala@capgemini.com>
2019-02-26 15:00:23 +0530

    Add project files.

---
+++ DropTables.sql
@@ -0,0 +1,20 @@
+drop table Project1.City
+drop table Project1.Users
+drop table Project1.States
+drop table Project1.Seller
+drop table Project1.Buyer
+drop table Project1.Images
+drop table Project1.Cart
+drop table Project1.Property
+
+
+
+select * from  Project1.Users
+select * from project1.Seller
+select * from Project1.Buyer
+select * from Project1.Cart
+select * from project1.Seller
+select * from project1.images
+select * from Project1.Property
+select * from Project1.City
+select * from Project1.States
---
+++ EasyHousingSolutionPL/HomePage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EasyHousingSolutionPL
+{
+    /// <summary>
+    /// Interaction logic for HomePage.xaml
+    /// </summary>
+    public partial class HomePage : Window
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            SupportPage sup = new SupportPage();
+            
+            sup.Show();
+        }
+
+        private void Hyperlink_Click_1(object sender, RoutedEventArgs e)
+        {
+            HomeLoans hom = new HomeLoans();
+            
+            hom.Show();
+        }
+
+        private void Hyperlink_Click_2(object sender, RoutedEventArgs e)
+        {
+            Login up = new Login();
+          
+            up.Show();
+            
+                
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //HomePage hp = new HomePage();
+            //hp.Close();
+            Login bs = new Login();
+            
+            bs.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Registration obj = new Registration();
+            
+            obj.Show();
+        }
+    }
+}
