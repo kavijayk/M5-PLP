346a38262d225b89d1046f5b40591e9315d93c8d
Vijay Kumar <karri-santosh.vijay-kumar@capgemini.com>
2019-02-26 14:53:02 +0530

    Add project files.

---
+++ EasyHousingSolutionPL/SupportPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EasyHousingSolutionPL
+{
+    /// <summary>
+    /// Interaction logic for SupportPage.xaml
+    /// </summary>
+    public partial class SupportPage : Window
+    {
+        public SupportPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            HomeLoans loans = new HomeLoans();
+            loans.Show();
+        }
+
+        private void Hyperlink_Click_1(object sender, RoutedEventArgs e)
+        {
+            Login man = new Login();
+            man.Show();
+        }
+
+        private void Hyperlink_Click_2(object sender, RoutedEventArgs e)
+        {
+            HomePage fromSup = new HomePage();
+            fromSup.Show();
+        }
+    }
+}
